2ccb4f2c1d87e1c94468c824f2f5126e49e4271d
Yasmin358 <amorin.yasmin378@gmail.com>
2023-09-12 13:02:21 -0300

    test css

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 // import '../styles/projectCard.css';
 import '../styles/cardProject.css';
 import github from '../images/icons/github-white.png';
